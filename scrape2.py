6a493a0bde12159cdb60e583c17580abf1c236b5
ashish <ashish.dutt8@gmail.com>
2016-10-15 17:23:27 +0800

    initial commit

---
+++ scrape2.py
@@ -0,0 +1,14 @@
+# this crawler takes in a user input and then crawls the whole website searching for url's in it and dumps them to a file
+# Source: http://null-byte.wonderhowto.com/news/basic-website-crawler-python-12-lines-code-0132785/
+import re, urllib
+
+textfile = file('depth_1.txt','wt')
+print "Enter the URL you wish to crawl.."
+print 'Usage  - "http://phocks.org/stumble/creepy/" <-- With the double quotes'
+myurl = input("@> ")
+for i in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(myurl).read(), re.I):
+        print i
+        for ee in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(i).read(), re.I):
+                print ee
+                textfile.write(ee+'\n')
+textfile.close()
